fc88ebd4a77ee331a94864e9737928f5b79e0e07
KeiranT <92025815+KeiranT@users.noreply.github.com>
2025-09-29 17:39:54 +0400

    created availability component

---
+++ src/app/features/availability/availability.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-availability',
+  imports: [],
+  templateUrl: './availability.html',
+  styleUrl: './availability.css'
+})
+export class Availability {
+
+}
